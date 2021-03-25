a771adb60c995179a6f97804c42cb85fdab620ad
Omar Ramadan <ramomar1992@outlook.com>
2021-03-25 15:21:34 +0200

    useing new port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('./public'));
 
@@ -23,4 +23,4 @@ app.get('/data', (request, response) => {
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
